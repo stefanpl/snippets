2f2fb86f752cb4c934f5f209d9b9506a47c10bbd
Stefan Plösser <stefan@rarelime.de>
2019-02-05 12:15:11 +0100

    fetch remote port, create http server, more bash aliases

---
+++ create-http-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Quickly spin up an http server
+
+npx http-server [public directory] -p 9283
---
+++ ssh-tunnel-route-connections-to-local-machine.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script fetches all connections which are made to a given port on a remote machine.
+# It forwards those connections to the given host/port on the local machine.
+#
+# The script will:
+# 
+# -f go to background
+# -N not execute any ssh commands.
+# The -4 flag forces ssh to use IPV4, because it can fail when using V6
+
+remotePortToBeFetched=8123
+targetHost=127.0.0.1
+targetPort=8080
+
+sshServer=hcli
+
+ssh -4 -L ${remotePortToBeFetched}:${targetHost}:${targetPort} ${sshServer} -f -N
