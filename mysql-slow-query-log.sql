4e6e1fb52a12351053f4a1300d8cd2bb2039215f
Stefan Plösser <stefan@rarelime.de>
2019-03-11 16:19:10 +0100

    slow query log takes second fractions

--- mysql-slow-query-log.sql
+++ mysql-slow-query-log.sql
@@ -3,7 +3,7 @@ set global log_queries_not_using_indexes = 'ON'
 set global slow_query_log_file ='/var/log/mysql/slow-query.log';
 
 # how much time before a query gets logged? (in seconds): 
-set global long_query_time = 20;
+set global long_query_time = 3.5;
 
 # check variables
 show variables like '%slow%';
