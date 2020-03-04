afd1197f1275dfbc90ad13e546dfe1fdd6c7e9ba
Stefan Plösser <stefan@rarelime.de>
2020-03-04 15:57:33 +0100

    working redis docker

--- useful-docker-containers/redis.sh
+++ useful-docker-containers/redis.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
+# Spin off a redis container on the default port:
 docker run --name one-off-redis \
   -p 6379:6379 \
   -d redis
-  
+
+# Connect to the redis container:
+docker run \
+  -it --rm \
+  --link one-off-redis:redishost \
+  redis \
+  redis-cli \
+    -h redishost
