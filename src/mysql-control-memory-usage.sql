c582f25387b6c9ad7e20c6952572ba17f8586667
Stefan Plösser <stefan@rarelime.de>
2020-04-27 12:39:49 +0200

    more comments

--- src/mysql-control-memory-usage.sql
+++ src/mysql-control-memory-usage.sql
@@ -3,10 +3,12 @@ show variables where Variable_name = 'innodb_buffer_pool_size';
 
 # Set the value
 SET GLOBAL innodb_buffer_pool_size=268435456;
+# This will be 256 MB (256 * 1024 * 1024)
 
 ### Warning:
-# Only certain values will be allowed. If a non-compliant value is inserted,
-#  mysql produces a warning and adjusts the value, possibly leading to a higher
+# Only certain values will be allowed, e.g. multiples of 128 MB.
+# If a non-compliant value is inserted, mysql produces a warning and adjusts
+# the value automatically, possibly leading to a higher
 #  memory usage than intended! 
 #
 # Be sure to check the value (show variables) after setting it and observe
