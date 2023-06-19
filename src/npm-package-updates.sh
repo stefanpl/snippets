71090fdc14bf58579f54b4d900d0015815d4c427
Stefan Plösser <stefan@nvon.com>
2023-06-19 11:16:50 +0200

    Npm update packages

--- src/npm-package-updates.sh
+++ src/npm-package-updates.sh
@@ -2,7 +2,10 @@
 # https://www.npmjs.com/package/npm-check-updates
 
 # do a dry run, not performing major updates:
-npx npm-check-updates --target minor
+npx npm-check-updates --target major
 
 # add the -u flag to adjust package.json
 npx npm-check-updates --target minor -u
+
+# Find unused dependencies:
+npx depcheck
