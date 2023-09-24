9ded6f9d1feb3a6e7bba98e6aba3eeea6f6ff330
Stefan Plösser <stefan@nvon.com>
2023-09-24 22:42:55 +0200

    latest

--- src/npm-package-updates.sh
+++ src/npm-package-updates.sh
@@ -2,7 +2,7 @@
 # https://www.npmjs.com/package/npm-check-updates
 
 # do a dry run, not performing major updates:
-npx npm-check-updates --target major
+npx npm-check-updates --target latest
 
 # add the -u flag to adjust package.json
 npx npm-check-updates --target minor -u
